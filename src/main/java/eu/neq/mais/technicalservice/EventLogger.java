b426f6af26e8b0afc91bcb6e0a7652e304b6eee4
seba <seba@seba-pc.localdomain>
2011-11-27 12:10:34 +0100

    dasdasdd

--- src/main/java/eu/neq/mais/technicalservice/EventLogger.java
+++ src/main/java/eu/neq/mais/technicalservice/EventLogger.java
@@ -3,14 +3,20 @@ package eu.neq.mais.technicalservice;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
+import java.util.logging.Logger;
 
 /**
  * Later, we may want to write the event log to a file or something, not the system output stream :)
  * @author seba
  *
  */
-public abstract class EventLog {
+public abstract class EventLogger extends Logger {
 		
+	protected EventLogger(String arg0, String arg1) {
+		super("","");
+		
+	}
+
 	private static PrintStream eventLog = System.out;
 	
 	
@@ -19,13 +25,13 @@ public abstract class EventLog {
 	}
 
 	public static void setEventLog(PrintStream eventLog) {
-		EventLog.eventLog = eventLog;
+		EventLogger.eventLog = eventLog;
 		init();
 	}
 	
 	public static void setEventLog(String fileName) {
 		try {
-			EventLog.eventLog = new PrintStream(new File(fileName));
+			EventLogger.eventLog = new PrintStream(new File(fileName));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
