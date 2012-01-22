658c7324f0f8843352a1e6cca8206c2e79eab4e1
seba <seba@seba-pc.localdomain>
2012-01-22 01:30:53 +0100

    User interface

---
+++ src/main/java/eu/neq/mais/domain/User.java
@@ -0,0 +1,5 @@
+package eu.neq.mais.domain;
+
+public interface User {
+
+}
---
+++ src/main/java/eu/neq/mais/domain/gnuhealth/UserGnu.java
@@ -0,0 +1,36 @@
+package eu.neq.mais.domain.gnuhealth;
+
+import eu.neq.mais.domain.User;
+
+public class UserGnu implements User {
+
+	String name;
+	String id;
+	String login;
+	String rec_name;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getRec_name() {
+		return rec_name;
+	}
+	public void setRec_name(String rec_name) {
+		this.rec_name = rec_name;
+	}
+}
