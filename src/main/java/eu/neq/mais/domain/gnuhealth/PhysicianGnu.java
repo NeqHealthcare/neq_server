62857c1623db718e846cba3f8895a12317118556
seba <seba@seba-pc.localdomain>
2012-01-22 22:45:21 +0100

    adding two domain classes

---
+++ src/main/java/eu/neq/mais/domain/gnuhealth/PhysicianGnu.java
@@ -0,0 +1,23 @@
+package eu.neq.mais.domain.gnuhealth;
+
+import eu.neq.mais.domain.Physician;
+
+public class PhysicianGnu implements Physician {
+	
+	String id;
+	String internal_user;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getInternal_user() {
+		return internal_user;
+	}
+	public void setInternal_user(String internal_user) {
+		this.internal_user = internal_user;
+	}
+	
+}
