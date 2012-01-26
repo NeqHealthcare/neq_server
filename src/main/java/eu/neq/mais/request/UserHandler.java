81d8c23a280e8f30db29fc72e41cc464b63c8912
J.G. <jgansen@mail.uni-mannheim.de>
2012-01-26 21:15:23 +0100

    Started patient/user image implementation

--- src/main/java/eu/neq/mais/request/UserHandler.java
+++ src/main/java/eu/neq/mais/request/UserHandler.java
@@ -1,16 +1,20 @@
 package eu.neq.mais.request;
 
+import java.io.File;
 import java.util.logging.Logger;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import eu.neq.mais.connector.Connector;
 import eu.neq.mais.connector.ConnectorFactory;
 import eu.neq.mais.technicalservice.SessionStore;
+import javax.activation.MimetypesFileTypeMap;
 
 /**
  * 
@@ -34,7 +38,7 @@ public class UserHandler {
 	@GET
 	@Path("/personalInformation")
 	@Produces(MediaType.APPLICATION_JSON)
-	public String returnAUsersPatients(@QueryParam("session") String session,
+	public String returnPersnoalData(@QueryParam("session") String session,
 			@QueryParam("picture") String picture,
 			@QueryParam("name") String name) {
 
@@ -52,4 +56,31 @@ public class UserHandler {
 		return personalInformation;
 
 	}
+
+	@GET
+	@Path("/image/")
+	@Produces("image/*")
+	public Response returnImage(@QueryParam("session") String session, @QueryParam("size") String size){
+		String imagePath = "false";
+		File image = new File(imagePath);
+		
+		if(!image.exists()){
+			throw new WebApplicationException(404);
+		}
+		
+		try {
+			connector = ConnectorFactory.getConnector(SessionStore
+					.getBackendSid(session));
+//			imagePath = connector.getImage(session,size);
+		} catch (Exception e) {
+			e.printStackTrace();
+			imagePath = "false";
+		}
+		String mt = new MimetypesFileTypeMap().getContentType(image);
+
+		logger.info("return image for a specific user: ");
+		return Response.ok(image, mt).build();
+	}
+	
 }
+
