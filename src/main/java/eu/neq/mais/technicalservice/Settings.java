4889bace159db0e4fef0dcfc3a9e9618b4958e79
dstohr <dennystohr@gmail.com>
2012-04-08 18:36:17 +0200

    change of image request

--- src/main/java/eu/neq/mais/technicalservice/Settings.java
+++ src/main/java/eu/neq/mais/technicalservice/Settings.java
@@ -6,7 +6,7 @@ public class Settings {
     public static final String SSL_KEYFILE = "./sec/neq_keystore";
     public static final String LOG_FILE_MAIN = "./logs/main.log";
     public static final String LOG_FILE_CONNECTORS = "./logs/connectors.log";
-    public static final String ALLOW_ORIGIN_ADRESS = "http://localhost:10920";
+    public static final String ALLOW_ORIGIN_ADRESS = "*";
 
 
 }
