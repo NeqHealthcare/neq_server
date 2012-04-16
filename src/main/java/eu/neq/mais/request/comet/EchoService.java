20eeb8bf04f3d8dc429a4b1e674981fa9a30751f
seba <seba@seba-pc.localdomain>
2012-04-16 21:48:34 +0200

    for c.t.

--- src/main/java/eu/neq/mais/request/comet/EchoService.java
+++ src/main/java/eu/neq/mais/request/comet/EchoService.java
@@ -3,23 +3,36 @@ package eu.neq.mais.request.comet;
 import java.util.Map;
 
 import org.cometd.bayeux.server.BayeuxServer;
+import org.cometd.bayeux.server.ConfigurableServerChannel;
+import org.cometd.bayeux.server.ServerMessage;
 import org.cometd.bayeux.server.ServerSession;
+import org.cometd.java.annotation.Configure;
+import org.cometd.java.annotation.Listener;
+import org.cometd.java.annotation.Service;
+import org.cometd.java.annotation.Session;
 import org.cometd.server.AbstractService;
+import org.cometd.server.authorizer.GrantAuthorizer;
 
+@Service("echoService")
+public class EchoService {
 
-public class EchoService extends AbstractService {
+	 @Session
+     private ServerSession _session;
 
-	public EchoService(BayeuxServer bayeux) {
-		super(bayeux, "echo");
-		addService("/echo", "processEcho");
-		
-	}
-	
-	public void processEcho(ServerSession remote, Map<String, Object> data) {
-		remote.deliver(getServerSession(), "/echo", data, null);
-		
-		
-	}
+     @SuppressWarnings("unused")
+     @Configure("/service/echo")
+     private void configureEcho(ConfigurableServerChannel channel)
+     {
+         channel.addAuthorizer(GrantAuthorizer.GRANT_ALL);
+     }
+
+     @Listener("/service/echo")
+     public void doEcho(ServerSession session, ServerMessage message)
+     {
+         Map<String,Object> data = message.getDataAsMap();
+         System.out.println("ECHO from "+session+" "+data);
+         session.deliver(_session, message.getChannel(), data, null);
+     }
 
 
 }
--- src/main/java/eu/neq/mais/request/comet/HelloWorld.java
+++ src/main/java/eu/neq/mais/request/comet/HelloWorld.java
@@ -1,10 +1,15 @@
 package eu.neq.mais.request.comet;
 
 import org.cometd.bayeux.Bayeux;
+import org.cometd.bayeux.server.BayeuxServer;
+import org.cometd.java.annotation.AnnotationCometdServlet;
 import org.cometd.server.CometdServlet;
+import org.cometd.server.DefaultSecurityPolicy;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.bio.SocketConnector;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
@@ -13,35 +18,63 @@ public class HelloWorld {
 
 	public static void main(String[] args) throws Exception {
 
+		int port = 8081;
 		Server server = new Server();
+				
 		QueuedThreadPool qtp = new QueuedThreadPool();
 		qtp.setMinThreads(5);
 		qtp.setMaxThreads(200);
 		server.setThreadPool(qtp);
 		
 		SelectChannelConnector connector=new SelectChannelConnector();
-		connector.setPort(8081);
+		connector.setPort(port);
+        connector.setPort(port);
+        connector.setMaxIdleTime(120000);
+        connector.setLowResourcesMaxIdleTime(60000);
+        connector.setLowResourcesConnections(20000);
+        connector.setAcceptQueueSize(5000);
+        server.addConnector(connector);
+        SocketConnector bconnector=new SocketConnector();
+        bconnector.setPort(port+1);
+        server.addConnector(bconnector);
         server.addConnector(connector);
         
+       
+       
         ContextHandlerCollection contexts = new ContextHandlerCollection();
-        server.setHandler(contexts);
-
+        server.setHandler(contexts);      
 		ServletContextHandler context = new ServletContextHandler(contexts,
 				"/", ServletContextHandler.SESSIONS);
-
-		ServletHolder cometd_holder = new ServletHolder(CometdServlet.class);
-		cometd_holder.setInitParameter("timeout", "200000");
-		cometd_holder.setInitParameter("interval", "100");
-		cometd_holder.setInitParameter("maxInterval", "100000");
-		cometd_holder.setInitParameter("multiFrameInterval", "1500");
-		cometd_holder.setInitParameter("directDeliver", "true");
-		cometd_holder.setInitParameter("logLevel", "0");
-		cometd_holder.setInitOrder(1);
-		context.addServlet(cometd_holder, "/cometd/*");
+		server.setHandler(context);
 		
-		context.addEventListener(new BayeuxInitializer());
+		ServletHolder dftServlet = context.addServlet(DefaultServlet.class, "/");
+        dftServlet.setInitOrder(1);
+		
+		
+		
+        CometdServlet cometdServlet = new AnnotationCometdServlet();
+        ServletHolder comet = new ServletHolder(cometdServlet);
+        comet.setInitParameter("timeout", "200000");
+        comet.setInitParameter("interval", "100");
+        comet.setInitParameter("maxInterval", "100000");
+        comet.setInitParameter("multiFrameInterval", "1500");
+        comet.setInitParameter("directDeliver", "true");
+        comet.setInitParameter("logLevel", "1");
+       comet.setInitParameter("services","eu.neq.mais.request.comet.EchoService");
+        comet.setInitParameter("transports","org.cometd.websocket.server.WebSocketTransport");
+        comet.setInitOrder(2);
+        context.addServlet(comet, "/cometd/*");
+        
+//        ServletHolder demo=context.addServlet(CometdDemoServlet.class, "/demo/*");
+//        demo.setInitOrder(3);
+		
+//		context.addServlet(ConfigurationServlet.class, "");
+	//	server.addEventListener(new BayeuxInitializer());
 		
 		server.start();
+		
+		 BayeuxServer bayeux = cometdServlet.getBayeux();
+	     bayeux.setSecurityPolicy(new DefaultSecurityPolicy());
 	
 		
 	}
